18205e4271229706e8026b1cf1c0e4b8aa97dad8
Hunter Schafer <hschafer@cs.washington.edu>
2016-11-18 17:19:07 -0800

    Add redirect to make new doc

--- src/main/java/com/networms/ClientController.java
+++ src/main/java/com/networms/ClientController.java
@@ -3,15 +3,28 @@ package com.networms;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Hunter on 11/18/16.
  */
 @Controller
-@RequestMapping("/editor")
 public class ClientController {
 
-    @RequestMapping({"", "/"})
-    public void landingPage(Model model) {
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public ModelAndView newDocument(HttpServletResponse httpServletResponse) {
+        // TODO: Establish connection to server and get new docId
+        String docId = "abcd1234";
+        return new ModelAndView("redirect:/editor?id=" + docId);
+    }
+
+    @RequestMapping(value="editor", method = RequestMethod.GET)
+    public void edit(@RequestParam(value="id", required=false, defaultValue="") String docId,
+                     Model model) {
+        model.addAttribute("docId", docId);
     }
 }
