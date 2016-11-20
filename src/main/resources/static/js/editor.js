a2d1ca2501a3af0a3637788070ae2418161023fe
Hunter Schafer <hschafer@cs.washington.edu>
2016-11-19 17:07:45 -0800

    Handle range delete

--- src/main/resources/static/js/editor.js
+++ src/main/resources/static/js/editor.js
@@ -12,21 +12,33 @@
         textbox.keypress(function(e) {
             var key = e.key;
             var position = e.target.selectionStart;
-            console.log("Input(\"" + key + "\", @" + position + ")", e);
+            /* TODO: We can potentially write the addition to the pending list so that it "merges"
+                     changes next to each other into one big change. That way we can get some bigger
+                     messages being sent */
+            console.log("Insert(\"" + key + "\", @" + position + ")", e);
         });
 
         textbox.keydown(function(e) {
-            // TODO: Handle selection delete, copy, paste
+            // TODO: Handle copy and paste?
             var index = -1;
-            if (e.keyCode === BACKSPACE_CODE && e.target.selectionStart > 0) {
-                index = e.target.selectionStart - 1;
-            } else if (e.keyCode === DELETE_CODE
-                    && e.target.selectionStart < this.val().length()) {
-                index = e.target.selectionStart
+            var length = 1;
+            if (e.keyCode === BACKSPACE_CODE || e.keyCode === DELETE_CODE) {
+                if (e.target.selectionStart != e.target.selectionEnd) {
+                    // Delete range
+                    index = e.target.selectionStart;
+                    length = e.target.selectionEnd - e.target.selectionStart;
+                } else if (e.keyCode === BACKSPACE_CODE && e.target.selectionStart > 0) {
+                    // Delete previous char
+                    index = e.target.selectionStart - 1;
+                } else if (e.keyCode === DELETE_CODE &&
+                        e.target.selectionStart < this.val().length()) {
+                    // Delete "this" char
+                    index = e.target.selectionStart
+                }
             }
 
             if (index >= 0) {
-                console.log("Delete(@" + index + ")");
+                console.log("Delete(" + length + ", @" + index + ")");
             }
         });
     }
