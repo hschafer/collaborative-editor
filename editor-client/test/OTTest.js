7ec44bb85970a25c2029baf80bd43fc4bae1a425
Andrew Dawson <dawsoa2@cs.washington.edu>
2016-12-01 18:12:46 -0800

    figured out basics of mocha

---
+++ editor-client/test/OTTest.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var ot = require('../public/js/andrew.js');
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when not present', function() {
+            assert.equal(8, ot.x);
+        });
+    });
+});
