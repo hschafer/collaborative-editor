d11f157d1d438ab99029c4b114ed73a0824c16ba
Andrew Dawson <dawsoa2@cs.washington.edu>
2016-12-02 15:05:04 -0800

    got testing set up

--- editor-client/test.js
+++ editor-client/test.js
@@ -4,8 +4,8 @@ import Insert from './app/js/insert';
 import Delete from './app/js/delete';
 
 describe('Hello World', function () {
-    it('should increment a value', function () {
+    it('should return the index', function () {
         var insert = new Insert(1, 2, 3, 4);
-        assert.equal(2, insert.index, "dog");
+        assert.equal(3, insert.index, "dog");
     });
 });
