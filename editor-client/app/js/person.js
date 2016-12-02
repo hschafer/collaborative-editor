f7023c615e1ea974012d3aeafd4b686818eeb75d
Andrew Dawson <dawsoa2@cs.washington.edu>
2016-12-02 09:59:49 -0800

    finally figured out to get tests to work with npm and babel

---
+++ editor-client/app/js/person.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;   
+    }
+
+    get() {
+        return this.firstName;
+    }
+}
--- editor-client/test/OTTest.js
+++ editor-client/test/OTTest.js
@@ -1,9 +1,10 @@
 var assert = require('assert');
-var ot = require('../public/js/andrew.js');
+var Person = require('../public/js/person.js');
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when not present', function() {
-            assert.equal(8, ot.x);
+            var andrew = new Person("andrew", "dawson");
+            assert.equal("andrew", andrew.get());
         });
     });
 });
